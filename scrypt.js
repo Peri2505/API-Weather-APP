f0193c9b9f269cd32f54e9008443a7ed34b1fd7f
Peri2505 <moldobaevaperi@yahoo.com>
2023-03-04 10:01:46 +0600

    added the search engine

--- scrypt.js
+++ scrypt.js
@@ -36,9 +36,25 @@ function displayTemperature(response) {
   dataElement.innerHTML = formatDate(response.data.dt + 1000);
 }
 
-let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
 
-//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=${apiKey}`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+// let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+
+// //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=${apiKey}`;
+// let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=metric`;
+
+// axios.get(apiUrl).then(displayTemperature);
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);

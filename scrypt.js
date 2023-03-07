22626f7b12774a833870e0d7becf634c4207e6de
Peri2505 <moldobaevaperi@yahoo.com>
2023-03-07 21:13:54 +0600

    display the forecast data using the API

--- scrypt.js
+++ scrypt.js
@@ -21,32 +21,43 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data);
+  let forecast = response.data.daily;
   let forcastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat"];
+  //let days = ["Thu", "Fri", "Sat"];
   let forecastHTML = `<div class="row">`;
 
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
               <div class="col-2">
                 <div class="weather-forecast-date">
-                ${day}
+                ${formatDay(forecastDay.dt)}
                 </div>
-                <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="42"> 
+                <img src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png" alt="" width="42"> 
                 <div class="weather-forcast-temp"> 
                 <span class="weather-forecast-temp-max">
-                   18°
+                   ${Math.round(forecastDay.temp.max)}°
                 </span>
                 <span class="weather-forecast-temp-min">
-                  12°
+                  ${Math.round(forecastDay.temp.min)}°
                 </span>
                 </div> 
               </div>
           `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -54,7 +65,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
+  //console.log(coordinates);
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&unit=metric`;
 
